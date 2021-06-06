d7934e739f7c67d66344ca9269a8f6ab95a4c81c
MuhammadSaadQadeer <saadqadeer59@gmail.com>
2021-06-06 13:28:29 +0500

    basic prototype read

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,11 +1,19 @@
+// @ts-nocheck
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { ChromeMessage, Sender } from "../types";
 import { getCurrentTabUId, getCurrentTabUrl } from "../chrome/utils";
 
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
+let editorRef = {}
+
 export const Home = () => {
   const [url, setUrl] = useState<string>("");
   const [responseFromContent, setResponseFromContent] = useState<string>("");
+
+  const [newSelection, setNewSelection] = useState("")
 
   let { push } = useHistory();
 
@@ -15,6 +23,10 @@ export const Home = () => {
     response: any
   ) => {
     console.log("ssss", chromeMessage);
+    setNewSelection(chromeMessage)
+
+    let prevData = editorRef.getData();
+    editorRef.setData(prevData.concat(chromeMessage) )
   };
 
   /**
@@ -79,6 +91,29 @@ export const Home = () => {
         onEditorStateChange={onEditorStateChange}
         editorState={editorText}
       /> */}
+
+      <CKEditor
+        editor={ClassicEditor}
+        data="<p>Hello from CKEditor 5!</p>"
+        onReady={(editor) => {
+          // You can store the "editor" and use when it is needed.
+          console.log("Editor is ready to use!", editor);
+          editorRef = editor
+          let prevData = editor.getData();
+          console.log({prevData})
+          editor.setData(prevData.concat(newSelection) )
+        }}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          console.log({ event, editor, data });
+        }}
+        onBlur={(event, editor) => {
+          console.log("Blur.", editor);
+        }}
+        onFocus={(event, editor) => {
+          console.log("Focus.", editor);
+        }}
+      />
     </div>
   );
 };
