b7006b2fe63bacf36411f512800f9f3e229033d5
MuhammadSaadQadeer <saadqadeer59@gmail.com>
2021-06-27 10:53:45 +0500

    add: making notes functionality completed

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -19,78 +19,86 @@ export const Home = () => {
   ) => {
     setNewSelection(chromeMessage);
 
-    let prevData = editorRef.getData();
+    let previousState = editorRef.getData();
 
-    editorRef.setData(prevData.concat(chromeMessage));
+    editorRef.setData(previousState.concat(chromeMessage));
   };
 
   /**
    * Get current URL
    */
   useEffect(() => {
+    //  localStorage.setItem("string", "string")
+    let res = localStorage.getItem("string");
+
     chrome.storage.sync.get(["key"], function (result) {
-      let prevData = editorRef.getData();
-      editorRef.setData(prevData.concat(result.key));
+      // Adding the formatted data in editor if any
+      let previousState = "";
+      if (res) {
+        previousState = editorRef.getData();
+        editorRef.setData(previousState.concat(res));
+      }
+
+      if (result.key) {
+        previousState = editorRef.getData();
+        editorRef.setData(previousState.concat(result.key));
+        localStorage.removeItem("string");
+        chrome.storage.sync.remove(["key"]);
+      }
     });
 
     chrome.runtime.onMessage.addListener(messagesFromReactAppListener);
 
     getCurrentTabUrl((url) => {
-      setUrl(url || "undefined");
+      //  setUrl(url || "undefined");
     });
   }, []);
 
-  const sendTestMessage = () => {
-    const message: ChromeMessage = {
-      from: Sender.React,
-      message: "Hello from React",
-    };
+  // const sendTestMessage = () => {
+  //   const message: ChromeMessage = {
+  //     from: Sender.React,
+  //     message: "Hello from React",
+  //   };
 
-    getCurrentTabUId((id) => {
-      id &&
-        chrome.tabs.sendMessage(id, message, (responseFromContentScript) => {
-          setResponseFromContent(responseFromContentScript);
-        });
-    });
-  };
+  //   getCurrentTabUId((id) => {
+  //     id &&
+  //       chrome.tabs.sendMessage(id, message, (responseFromContentScript) => {
+  //         setResponseFromContent(responseFromContentScript);
+  //       });
+  //   });
+  // };
 
-  const sendRemoveMessage = () => {
-    const message: ChromeMessage = {
-      from: Sender.React,
-      message: "delete logo",
-    };
+  // const sendRemoveMessage = () => {
+  //   const message: ChromeMessage = {
+  //     from: Sender.React,
+  //     message: "delete logo",
+  //   };
 
-    getCurrentTabUId((id) => {
-      id &&
-        chrome.tabs.sendMessage(id, message, (response) => {
-          setResponseFromContent(response);
-        });
-    });
-  };
+  //   getCurrentTabUId((id) => {
+  //     id &&
+  //       chrome.tabs.sendMessage(id, message, (response) => {
+  //         setResponseFromContent(response);
+  //       });
+  //   });
+  // };
 
   return (
     <div style={{ width: "500px", height: "300px" }}>
       <CKEditor
         editor={ClassicEditor}
-        data="<p>Hello from CKEditor 5!</p>"
+        // data={contentState}
         onReady={(editor) => {
           // You can store the "editor" and use when it is needed.
-          console.log("Editor is ready to use!", editor);
           editorRef = editor;
-          let prevData = editor.getData();
-          console.log({ prevData });
-          editor.setData(prevData.concat(newSelection));
+          editor.setData(editor.getData().concat(newSelection));
         }}
         onChange={(event, editor) => {
           const data = editor.getData();
           console.log({ event, editor, data });
+          localStorage.setItem("string", data);
         }}
-        onBlur={(event, editor) => {
-          console.log("Blur.", editor);
-        }}
-        onFocus={(event, editor) => {
-          console.log("Focus.", editor);
-        }}
+        onBlur={(event, editor) => {}}
+        onFocus={(event, editor) => {}}
       />
     </div>
   );
